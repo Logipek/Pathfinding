0db517947149c77f27aa0f723e140a690c29643a
code-nuage <eloi.hariot@protonmail.com>
2024-10-18 16:11:03 +0200

    fixing

--- main.c
+++ main.c
@@ -7,6 +7,6 @@
 // +--------+
 int main(int argc, char *argv[]) {
   FILE* file = fopen(argv[1], "r");
-  printf(); // 
+  printf("nombre de noeud(s): %d", nodes_quantity(file)); // nodes_quantity test
   return 0;
 }
