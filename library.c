360225999be78e84627b8b781bd3961b6735c5a7
code-nuage <eloi.hariot@protonmail.com>
2024-10-18 16:04:48 +0200

    Adds lib.c

--- library.c
+++ library.c
@@ -1,4 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+// +---------+
+// |  NODES  |
+// +---------+
 void parse_nodes_file(FILE* file) {
+    
+}
 
+int nodes_quantity(FILE* file) {
+  char buffer[256];
+  int i;
+  while (fgets(buffer, 256, file) != NULL) {
+    i++;
+  }
+  return i;
 }
