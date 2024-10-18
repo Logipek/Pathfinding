94709edf3f2c4d985415b6d28089d21dd0d72bbe
code-nuage <eloi.hariot@protonmail.com>
2024-10-18 15:31:42 +0200

    Changes library structure

--- library.h
+++ library.h
@@ -1,6 +1,18 @@
 #ifdef
 #define PATHFINDING
 
+typedef struct {
+    int id;
+    struct n **links;
+} Node;
+
+// +---------+
+// |  NODES  |
+// +---------+
 void parse_nodes_file(*FILE file);
+int nodes_quantity(*FILE file);
+int nodes_links_quantity(*FILE file);
+int node_start(*FILE file);
+int node_end(*FILE file);
 
 #endif
